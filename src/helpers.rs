2b2d9cb38ae95bc6f4bdfd593370c05d23add487
Stefan VDV <vdvman88@gmail.com>
2023-12-13 09:52:57 +1100

    feat: ✨ Add helper to try to convert ascii to a digit and to know if it worked

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,12 @@
+pub fn try_ascii_to_digit(c: u8) -> Option<u32> {
+    let digit = c.wrapping_sub(b'0');
+    if digit > 9 {
+        None
+    } else {
+        Some(digit as u32)
+    }
+}
+
 pub fn ascii_to_digit(c: u8) -> u32 {
     (c - b'0').into()
 }
